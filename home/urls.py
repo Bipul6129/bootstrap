53bacf405585457c976a6c72fb93b5582530252e
Bipul6129 <70795027+Bipul6129@users.noreply.github.com>
2022-08-12 10:54:11 +0545

    urls supdate

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
 
-
 urlpatterns = [
-    path('',views.home,name='home')
+    path('',views.landing_index,name='landing_index'),
+    path('learn_bike/',views.learn_bike,name='learn_bike'),
+    path('learn_car/',views.learn_car,name='learn_car'),
+    path('learn_excavator/',views.learn_excavator,name='learn_excavator'),
+    path('about_us/',views.about_us,name='about_us')
 ]
